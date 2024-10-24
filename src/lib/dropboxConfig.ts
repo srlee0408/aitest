c89e1950197597da613f1d7bdb14e9cc89fee794
SRLee <srlee@MacBook-Pro.local>
2024-10-24 17:34:05 +0900

    fix deploy issue

--- src/lib/dropboxConfig.ts
+++ src/lib/dropboxConfig.ts
@@ -1,21 +1,22 @@
 import { Dropbox } from 'dropbox';
 import { getEnvVariables } from './env';
 
-let dropbox: Dropbox;
+let dropbox: Dropbox | null = null;
 
-const { DROPBOX_ACCESS_TOKEN } = getEnvVariables();
-
-// 클라이언트 사이드에서만 Dropbox 인스턴스를 생성
-if (typeof window !== 'undefined') {
-  dropbox = new Dropbox({ 
-    accessToken: DROPBOX_ACCESS_TOKEN,
-    fetch: fetch.bind(window)
-  });
-} else {
-  // 서버 사이드에서는 기본 fetch 사용
-  dropbox = new Dropbox({ 
-    accessToken: DROPBOX_ACCESS_TOKEN
-  });
-}
-
-export default dropbox;
+export const getDropboxClient = async (): Promise<Dropbox> => {
+  if (!dropbox) {
+    const { DROPBOX_ACCESS_TOKEN } = await getEnvVariables();
+    
+    if (typeof window !== 'undefined') {
+      dropbox = new Dropbox({ 
+        accessToken: DROPBOX_ACCESS_TOKEN,
+        fetch: fetch.bind(window)
+      });
+    } else {
+      dropbox = new Dropbox({ 
+        accessToken: DROPBOX_ACCESS_TOKEN
+      });
+    }
+  }
+  return dropbox;
+};
